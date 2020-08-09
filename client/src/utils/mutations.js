8bec2277339bca441039eefb95ac64566b136ee8
unknown <asakthisner@hotmail.com>
2020-08-08 21:48:49 -0700

    added client utils mutations for create user and login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,34 @@
+// new -- GraphQL to gather changes to the client
 import gql from 'graphql-tag';
 
+
+// new - names to match /server's mutations resolvers
+
+// new -- login user mutation, two variables: email, password
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
-        _id
-        username
+            _id
+            username
       }
     }
   }
 `;
+
+// new -- add user mutation, three variables: email, pwd, username
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+            _id
+            username
+      }
+    }
+  }
+`;
+
+
+// new -- todo:  add save book mutation, from server/resolver
